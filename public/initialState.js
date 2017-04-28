78bf8af4cbe2665a8cd86469e82694ba149c5b41
Jad <jad.chalita@gmail.com>
2017-04-28 09:30:09 +0000

    Setting up an initial state with data for testing/dev purposes

---
+++ public/initialState.js
@@ -0,0 +1,30 @@
+const initialState = {  
+   "PF_stock_list":[  
+      {  
+         "ticker":"AAPL",
+         "quantity":100,
+         "last_available_price":143.79,
+         "currency":"USD"
+      },
+      {  
+         "ticker":"NFLX",
+         "quantity":100,
+         "last_available_price":153.08,
+         "currency":"USD"
+      },
+      {  
+         "ticker":"AMZN",
+         "quantity":200,
+         "last_available_price":918.38,
+         "currency":"USD"
+      },
+      {  
+         "ticker":"BNP",
+         "quantity":200,
+         "last_available_price":65.68,
+         "currency":"EUR"
+      }
+   ]
+}
+
+export default initialState

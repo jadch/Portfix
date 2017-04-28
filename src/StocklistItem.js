5422fd2afebbc227ca329a8532228f7ab065470d
Jad <jad.chalita@gmail.com>
2017-04-28 15:53:44 +0000

    Now diplaying percentage change in StocklistItem

--- src/StocklistItem.js
+++ src/StocklistItem.js
@@ -3,11 +3,15 @@ import React, { Component } from 'react'
 class StocklistItem extends Component {
   render() {
     const Stock = this.props.stock
+    const percentage_change = (((Stock.last_available_price - Stock.entry_price) / Stock.entry_price) * 100).toFixed(2)
+
     return (
       <div className='StocklistItem'>
         <p>{Stock.name} ({Stock.ticker})</p>
         <p>{Stock.quantity}</p>
+        <p>{Stock.entry_price}</p>
         <p>{Stock.last_available_price}</p>
+        <p>{percentage_change > 0 ? '+' + percentage_change + '%' : percentage_change + '%'}</p>
       </div>
     )
   }

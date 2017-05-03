b711632aaab37f604f569c8b14f5d2d21956ee04
Jad <jad.chalita@gmail.com>
2017-05-03 09:17:55 +0000

    Created ArraysToObjects to help format query data

---
+++ src/helper_functions/ArraysToObjects.js
@@ -0,0 +1,22 @@
+// This function takes in as input historical data from www.quandl.com Wiki EOD Stock Prices (https://www.quandl.com/data/WIKI-Wiki-EOD-Stock-Prices/documentation/documentation?modal=null)
+// The data, for each stock, is obtained from Quandl with the following format:
+// An array of arrays, with each array containing [date, adjusted_close, adjusted_volume]
+
+// This function takes the query data (array of arrays) and returns it under the following format:
+// an array of objects {date: xx, adjusted_close: xx, adjusted_volume: xx}
+
+const ArraysToObjects = (ArrayofArrays) => {
+  var return_array = [] //Array to be returned
+
+  ArrayofArrays.map( (array) => {
+    let temporary_obj = {}
+    temporary_obj['date'] = array[0]
+    temporary_obj['adjusted_close'] = array[1]
+    temporary_obj['adjusted_volume'] = array[2]
+
+    return_array.push(temporary_obj)
+  })
+  return return_array
+}
+
+export default ArraysToObjects

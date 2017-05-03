6a753878619603b4a4e4bbeab9d808bfa1fadeb3
Jad <jad.chalita@gmail.com>
2017-05-03 13:22:00 +0000

    Formatted the X Axis

--- src/StockDetailPage.js
+++ src/StockDetailPage.js
@@ -4,11 +4,23 @@ import ArraysToObjects from './helper_functions/ArraysToObjects.js'
 import queryData from '../public/queryData.js'
 import initialState from '../public/initialState.js'
 
+var dateFormat = require('dateformat')
+
 class StockDetailPage extends Component {
   render () {
     const ticker = this.props.match.params.ticker
     const stockInfo = initialState.Stock_info[ticker]
     const data = ArraysToObjects(queryData[ticker])
+
+    // tickFormat format the ticks on the XAxis. If january => indicate month + year
+    const tickFormat = (tick) => {
+      if( tick.slice(5,7) == '01') {
+        return dateFormat(tick, 'mmm yy')
+      } else {
+        return dateFormat(tick, 'mmm')
+
+      }
+    }
     return (
       <div>
         <div>
@@ -22,7 +34,7 @@ class StockDetailPage extends Component {
                 <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
               </linearGradient>
             </defs>
-            <XAxis dataKey='date' />
+            <XAxis dataKey='date' tickFormatter={tickFormat} interval={28}/>
             <YAxis />
             <CartesianGrid strokeDasharray="3 3" />
             <Tooltip />

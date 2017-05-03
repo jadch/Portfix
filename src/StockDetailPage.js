d57b826a22c4b9ec653b551b43601b53fa33ad85
Jad <jad.chalita@gmail.com>
2017-05-03 12:00:14 +0000

    StockDetailPage now displays graph with stock data in it

--- src/StockDetailPage.js
+++ src/StockDetailPage.js
@@ -1,21 +1,16 @@
 import React, { Component } from 'react'
 import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts'
+import ArraysToObjects from './helper_functions/ArraysToObjects.js'
+import queryData from '../public/queryData.js'
 
 class StockDetailPage extends Component {
   render () {
-    const data = [
-      {name: 'Page A', uv: 4000},
-      {name: 'Page B', uv: 3000},
-      {name: 'Page C', uv: 2000},
-      {name: 'Page D', uv: 2780},
-      {name: 'Page E', uv: 1890},
-      {name: 'Page F', uv: 2390},
-      {name: 'Page G', uv: 3490}
-      ]
+    const ticker = this.props.match.params.ticker
+    const data = ArraysToObjects(queryData[ticker])
     return (
       <div>
         <div>
-          <h2>{this.props.match.params.ticker}</h2>
+          <h2>{ticker}</h2>
         </div>
         <div>
           <AreaChart width={700} height={300} data={data}>
@@ -25,11 +20,11 @@ class StockDetailPage extends Component {
                 <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
               </linearGradient>
             </defs>
-            <XAxis dataKey='name' />
+            <XAxis dataKey='date' />
             <YAxis />
             <CartesianGrid strokeDasharray="3 3" />
             <Tooltip />
-            <Area type='monotone' dataKey='uv' stroke='#8884d8' fillOpacity={1} fill='url(#adjustedClose)' />
+            <Area type='monotone' dataKey='adjusted_close' stroke='#8884d8' fillOpacity={1} fill='url(#adjustedClose)' />
           </AreaChart>
         </div>
       </div>

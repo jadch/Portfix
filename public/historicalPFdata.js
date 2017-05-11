64b75149f693da18bfdcfcbfd45fa74992042aca
Jad <jad.chalita@gmail.com>
2017-05-11 16:00:57 +0000

    Created historicalPFdata.js to store the history of a user's PF

---
+++ public/historicalPFdata.js
@@ -0,0 +1,23 @@
+// HistoricalPFdata will hold the history of the client portfolio. The client shouldn't be able to modify the history of
+// his or her portfolio. Not to be used to simulate a portfolio.
+
+// Data stucture:
+// {
+//   'aaaa-mm-dd': {
+//     'stocks': {
+//       'ticker': {
+//         'quantity': xxx,
+//       }
+//     },
+//     'cash': xxx
+//   },
+// }
+
+// The reason the stock price is not included in the data is because the calculated adjusted close changes
+// in the event of a split, or dividends.
+
+const historicalPFdata = {
+
+}
+
+module.exports = historicalPFdata

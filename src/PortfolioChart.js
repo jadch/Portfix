0c701d138ee0ef0fe9d018b0637c92873e8f0476
Jad <jad.chalita@gmail.com>
2017-05-19 09:06:57 +0000

    Formatted X-axis ticks in PortfolioChart

--- src/PortfolioChart.js
+++ src/PortfolioChart.js
@@ -3,11 +3,22 @@ import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts'
 import portfolioValuation from './helper_functions/portfolioValuation.js'
 import test_historicalPFdata from './test_scripts/test_historicalPFdata.js'
 import object_to_array from './helper_functions/object_to_array.js'
+var dateFormat = require('dateformat')
 
 class PortfolioChart extends Component {
   render () {
     var PF_valuation = portfolioValuation(test_historicalPFdata)
     const data = object_to_array(PF_valuation)
+
+    // tickFormat formats the ticks on the XAxis. If january => indicate month + year
+    const tickFormat = (tick) => {
+      if (tick.slice(5, 7) === '01') {
+        return dateFormat(tick, 'mmm yy')
+      } else {
+        return dateFormat(tick, 'mmm')
+      }
+    }
+
     return (
       <div>
         <AreaChart width={700} height={300} data={data}>
@@ -17,11 +28,11 @@ class PortfolioChart extends Component {
               <stop offset='95%' stopColor='#8884d8' stopOpacity={0} />
             </linearGradient>
           </defs>
-          <XAxis dataKey='date' interval={28} />
+          <XAxis dataKey='date' interval={28} tickFormatter={tickFormat}/>
           <YAxis />
           <CartesianGrid strokeDasharray='3 3' />
           <Tooltip />
-          <Area type='monotone' dataKey='value' name='PF Value' stroke='#8884d8' fillOpacity={1} fill='url(#value)' />
+          <Area type='monotone' dataKey='value' name='Portfolio Value' stroke='#8884d8' fillOpacity={1} fill='url(#value)' />
         </AreaChart>
       </div>
     )
